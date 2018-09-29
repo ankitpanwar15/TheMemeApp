77cec0d7544cd44ba00f066ed8c186110655185e
Ashish <akuzi770@gmail.com>
2018-09-29 13:21:58 +0530

    Login/Signup error

--- app/src/main/java/com/example/ashish/thememeapp/MainActivity.java
+++ app/src/main/java/com/example/ashish/thememeapp/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.ashish.thememeapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //FirebaseApp.initializeApp(this);
     }
 
 
--- app/src/main/java/com/example/ashish/thememeapp/SignupPage.java
+++ app/src/main/java/com/example/ashish/thememeapp/SignupPage.java
@@ -1,18 +1,55 @@
 package com.example.ashish.thememeapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignupPage extends AppCompatActivity {
+
+    private FirebaseAuth authObj;
+    private EditText textEmail;
+    private EditText textPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+
+        //FirebaseApp.initializeApp(this);
+
+        textEmail = findViewById(R.id.editMail);
+        textPassword = findViewById(R.id.editPassword);
+
+
+        Toast.makeText(SignupPage.this, "bef",
+                Toast.LENGTH_SHORT).show();
+
+        authObj = FirebaseAuth.getInstance();
+
+        Toast.makeText(SignupPage.this, "aff",
+                Toast.LENGTH_SHORT).show();
+
     }
+
+    /*@Override
+    protected void onStart() {
+        super.onStart();
+        if(authObj.getCurrentUser() != null){
+            FirebaseUser currentUser = authObj.getCurrentUser();
+            //Continue to next page
+        }
+    }*/
 
     void clickCall(View view){
         switch (view.getId()){
@@ -22,10 +59,34 @@ public class SignupPage extends AppCompatActivity {
                 break;
 
             case R.id.btnSignup :
+                signUper(textEmail.getText().toString(),textPassword.getText().toString());
                 Toast.makeText(this, "Signin....", Toast.LENGTH_SHORT).show();
                 break;
 
         }
+    }
+
+    void signUper(String email, String password){
+
+        authObj.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("ASH", "signInWithEmail:success");
+                            FirebaseUser user = authObj.getCurrentUser();
+                            Toast.makeText(SignupPage.this, "Authentication Successful.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("ASH", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(SignupPage.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
 
 
     }
